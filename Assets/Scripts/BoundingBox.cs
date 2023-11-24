5270f96ac69fbabce450d114d3ada4c4e616c597
MateoMonastra <mateomonastravm@gmail.com>
2023-11-23 21:08:11 -0300

    Created MyBounds struct

--- Assets/Scripts/BoundingBox.cs
+++ Assets/Scripts/BoundingBox.cs
@@ -1,16 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 
 public class BoundingBox : MonoBehaviour
 {
     [SerializeField] private List<Vector3> vertices = new List<Vector3>();
-    private Bounds boundingBox;
+    public struct MyBounds
+    {
+        private Vector3 m_Center;
+
+        private Vector3 m_Extents;
+
+        // Resumen:
+        //     The center of the bounding box.
+        public Vector3 center
+        {
+            get
+            {
+                return m_Center;
+            }
+           
+            set
+            {
+                m_Center = value;
+            }
+        }
+
+        // Resumen:
+        //     The total size of the box. This is always twice as large as the extents.
+        public Vector3 size
+        {
+            get
+            {
+                return m_Extents * 2f;
+            }
+            
+            set
+            {
+                m_Extents = value * 0.5f;
+            }
+        }
+
+        public MyBounds(Vector3 center, Vector3 size)
+        {
+            m_Center = center;
+            m_Extents = size * 0.5f;
+        }
+    }
+
+    private MyBounds boundingBox;
 
     private void Start()
     {
         SetVertices();
-        DrawBoundingBox();
     }
 
     private void Update()
@@ -39,11 +84,11 @@ public class BoundingBox : MonoBehaviour
         return vertices;
     }
 
-    Bounds CalculateBoundingBox(List<Vector3> vertexList)
+    MyBounds CalculateBoundingBox(List<Vector3> vertexList)
     {
         if (vertexList.Count == 0)
         {
-            return new Bounds(transform.position, Vector3.zero);
+            return new MyBounds(transform.position, Vector3.zero);
         }
 
         Vector3 min = vertexList[0];
@@ -58,7 +103,7 @@ public class BoundingBox : MonoBehaviour
         Vector3 size = max - min;
         Vector3 center = (max + min) / 2;
 
-        return new Bounds(center, size);
+        return new MyBounds(center, size);
     }
 
     void DrawBoundingBox()
@@ -66,8 +111,11 @@ public class BoundingBox : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boundingBox.center, boundingBox.size);
     }
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     { 
-        DrawBoundingBox();
+        if (Application.isPlaying)
+        {
+            DrawBoundingBox();
+        }
     }
 }
